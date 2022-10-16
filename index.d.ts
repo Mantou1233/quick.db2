12eb2c2f4e20a2c6bf91fd30a191f5fdca7ea8fb
Mantou1233 <rwkn100@gmail.com>
2022-10-16 21:04:05 +0800

    fix(typing): typing fixes v2

--- index.d.ts
+++ index.d.ts
@@ -1,29 +1,11 @@
 import * as Database from "better-sqlite3";
+import { Table } from "./src/class/table";
 
 declare module 'quick.db2' {
-	interface Fn {
+	export interface Fn {
 		(table: string): Table;
 		init: (
 			fileName: string
 		) => Table
 	}
-}
-
-declare class Table {
-	db: Database.Database;
-	table: string;
-	constructor(db: Database.Database, table: string);
-	all(): {
-		key: string;
-		value: any;
-	}[];
-	get(key: string): any;
-	set(key: string, value: any): any;
-	delete(key: string): boolean;
-	clear(): number | null;
-	has(key: string, value: number): boolean;
-	operate(key: string, value: number): void;
-	add(key: string, value: number): any;
-	subtract(key: string, value: number): any;
-	push(key: string, ...values: any[]): any;
 }
---
+++ tests/type.ts
@@ -0,0 +1,1 @@
+import db from "quick.db2";
